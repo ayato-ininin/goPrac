fe3aa90ce474d6ef2b2f9b3e13080ea386217464
ayato yamauchi <>
2022-11-06 16:35:31 +0900

    section11

--- main.go
+++ main.go
@@ -42,8 +42,11 @@ import (
 	"sync"
 	"sort"
 	"regexp"
-	_ "github.com/markcheno/go-quote"
+	_  "github.com/markcheno/go-quote"
 	_ "github.com/markcheno/go-talib"
+	"github.com/gorilla/websocket"
+	"golang.org/x/sync/semaphore"
+	"gopkg.in/ini.v1"
 )
 
 //module配下の指定が必要
@@ -64,6 +67,7 @@ func main() {
 	section8()
 	section9()
 	section10()
+	section11()
 }
 
 /*
@@ -1457,4 +1461,113 @@ func hmacPrac()  {
 
 	Server(apiKey, sign,data)
 
+}
+
+//サードパーティのパッケージ
+func section11()  {
+	semaphorePrac()//ごルーチンの同時実行数を制御する。セマフォ。
+	iniPrac()//configの設定ファイルを読み込むパッケージ
+	talibPrac()//株価分析
+	websocketPrac()
+}
+
+var s *semaphore.Weighted = semaphore.NewWeighted(1)//ごルーチンの最大実行数
+
+func longProcess2(ctx context.Context)  {
+	//一つのごルーチンが走ってればほかはキャンセル。
+	isAcquire := s.TryAcquire(1)
+	if !isAcquire {
+		fmt.Println("could not get lock")
+		return
+	}
+
+	// if err := s.Acquire(ctx,1);err != nil{
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	defer s.Release(1)
+	fmt.Println("Wait...")
+	time.Sleep(1* time.Second)
+	fmt.Println("Done")
+}
+
+// go get golang.org/x/sync/semaphore
+//ごルーチンの同時実行数を制御する。セマフォ。
+func semaphorePrac()  {
+	ctx := context.TODO()
+	go longProcess2(ctx)
+	go longProcess2(ctx)
+	go longProcess2(ctx)
+	time.Sleep(5* time.Second)
+}
+
+type ConfigList struct{
+	Port int
+	DbName string
+	SQLDriver string
+}
+
+var Config ConfigList
+
+func init()  {
+	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList{
+		Port : cfg.Section("web").Key("port").MustInt(),//なければ0
+		DbName : cfg.Section("db").Key("name").MustString("example.sql"),//なければ初期値が入る。
+		SQLDriver: cfg.Section("db").Key("driver").String(),//なければ空
+	}
+}
+//go get gopkg.in/ini.v1
+func iniPrac()  {
+	fmt.Printf("%T %v\n", Config.Port, Config.Port)
+	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
+	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
+}
+
+func talibPrac()  {
+	// spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	// fmt.Print(spy.CSV())
+	// rsi2 := talib.Rsi(spy.Close, 2)
+	// fmt.Println(rsi2)
+	// mva := talib.Ema(spy.Close, 14)//closeしたときの値を、過去１４日間のデータで計算moving average
+	// fmt.Println(mva)
+}
+
+type JsonRPC2 struct {
+	Version string `json:"jsonrpc"`
+	Method string `json:"method"`
+	Params interface{} `json:"params"`
+	Result interface{} `json:"result,omitempty"`
+	Id *int `json:"id,omitempty"`
+}
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
+func websocketPrac()  {
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
+		log.Fatal("subscribe:", err)
+		return
+	}
+
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Println("read:", err)
+			return
+		}
+
+		if message.Method == "channelMessage" {
+			log.Println(message.Params);
+		}
+	}
 }
